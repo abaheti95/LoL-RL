952a2e3c0d579e2ce15499f02fc724ccd94f6072
Ashutosh Baheti <ashutoshb@mosaic-cirrascale-02.reviz.ai2.in>
2023-10-15 11:00:55 -0700

    Fixed the data cleaning script

--- data_cleaning.py
+++ data_cleaning.py
@@ -28,40 +28,10 @@ def main():
     assert len(cleaner_train_data) == len(train_dataset) - len(merged_indices)
     # Save the cleaner_train_data
     save_file = os.path.join(data_dir, "cleaner_train.json")
-    # save_in_jsonl(cleaner_train_data, save_file)
-    # logging.info(f"Saved {len(cleaner_train_data)} samples to {save_file}")
+    save_in_jsonl(cleaner_train_data, save_file)
+    logging.info(f"Saved {len(cleaner_train_data)} samples to {save_file}")
     # find suffixes that contain a colon
     # cleaner_colon_suffixes = [datum['suffix'][0] for datum in cleaner_train_data if ":" in datum['suffix'][0]]
-
-    # Further data analysis
-    good_clean_suffixes = [datum['suffix'][0] for datum in cleaner_train_data]
-    bad_clean_suffixes = [datum['suffix'][1] for datum in cleaner_train_data]
-    import numpy as np
-    avg_good_clean_suffixes_len = np.mean([len(suffix.strip().split()) for suffix in good_clean_suffixes])
-    avg_bad_clean_suffixes_len = np.mean([len(suffix.strip().split()) for suffix in bad_clean_suffixes])
-    logging.info(f"Average length of good suffixes: {avg_good_clean_suffixes_len:.3f} vs bad suffixes: {avg_bad_clean_suffixes_len:.3f}")
-    # How many "I'm sorry" suffixes are there?
-    good_clean_im_sorry_suffixes = [suffix for suffix in good_clean_suffixes if "I'm sorry" in suffix]
-    bad_clean_im_sorry_suffixes = [suffix for suffix in bad_clean_suffixes if "I'm sorry" in suffix]
-    logging.info(f"Number of good suffixes that contain 'I'm sorry': {len(good_clean_im_sorry_suffixes)}/{len(good_clean_suffixes)}")
-    logging.info(f"Number of bad suffixes that contain 'I'm sorry': {len(bad_clean_im_sorry_suffixes)}/{len(bad_clean_suffixes)}")
-    def SP(phrase):
-        good_clean_phrase_suffixes = [suffix for suffix in good_clean_suffixes if phrase in suffix]
-        bad_clean_phrase_suffixes = [suffix for suffix in bad_clean_suffixes if phrase in suffix]
-        logging.info(f"Number of good suffixes that contain {phrase}: {len(good_clean_phrase_suffixes)}/{len(good_clean_suffixes)}")
-        logging.info(f"Number of bad suffixes that contain {phrase}: {len(bad_clean_phrase_suffixes)}/{len(bad_clean_suffixes)}")
-    breakpoint()
-
-    dpo_dpo_eval = os.path.join("dpo_dpo_cache", "eval_cache.pkl")
-    all_val_gen_rewards, prefixes, fixed_prefixes, gen_suffixes = load_from_pickle(dpo_dpo_eval)
-    # Percentiles of all_val_gen_rewards
-    percentiles = [0, 10, 25, 50, 75, 90, 100]
-    logging.info(f"Percentiles = {percentiles}")
-    logging.info(f"Value percentiles: {np.percentile(all_val_gen_rewards, percentiles)}")
-    sorted_rewards = sorted(all_val_gen_rewards, reverse=True)
-    rps = [(reward, {"prefix":prefix, "suffix":suffix}) for reward, prefix, suffix in zip(all_val_gen_rewards, prefixes, gen_suffixes)]
-    # Sort rps by reward in descending order
-    rps.sort(key=lambda x: x[0], reverse=True)
 
 
 
